a342953141b386751ea4394b3bf7562ed5d0397e
kaperskyguru <solomoneseme@gmail.com>
2020-10-01 13:06:48 +0100

    Fixed JS mix bug

--- resources/views/spa.blade.php
+++ resources/views/spa.blade.php
@@ -1,37 +1,40 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
-        @if(config('EVENT_TYPE_THEME') == 'flofest')
-            <title>Flofest</title>
-        @else
-            <title>{{ config('app.name', 'Laravel') }}</title>
-            <link rel="icon" href="{{ asset('img/logo-1-150x150.png') }}" sizes="32x32" />
-            <link rel="icon" href="{{ asset('img/logo-1-300x300.png') }}" sizes="192x192" />
-            <link rel="apple-touch-icon-precomposed" href="{{ asset('img/logo-1-300x300.png') }}" />
-            <meta name="msapplication-TileImage" content="{{ asset('img/logo-1-300x300.png') }}" />
-        @endif
-        
 
-        <link href="{{ asset('css/normalize.css?v=1.1') }}" rel="stylesheet">
-        <link href="{{ asset('css/appv1.css?v=1.1') }}" rel="stylesheet">
-    </head>
-    <body class="theme-{{ config('EVENT_TYPE_THEME', 'htf') }}">
-        <div id="app">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    @if(config('EVENT_TYPE_THEME') == 'flofest')
+    <title>Flofest</title>
+    @else
+    <title>{{ config('app.name', 'Laravel') }}</title>
+    <link rel="icon" href="{{ asset('img/logo-1-150x150.png') }}" sizes="32x32" />
+    <link rel="icon" href="{{ asset('img/logo-1-300x300.png') }}" sizes="192x192" />
+    <link rel="apple-touch-icon-precomposed" href="{{ asset('img/logo-1-300x300.png') }}" />
+    <meta name="msapplication-TileImage" content="{{ asset('img/logo-1-300x300.png') }}" />
+    @endif
 
-            <app></app>
-        </div>
-        <script>
-            window.locale = '{{ app()->getLocale() }}';
-            
-            window.appConfigs = {
-                'theme': "{{ config('EVENT_TYPE_THEME', 'htf') }}"
-            }
-        </script>
-        
-        <script src="{{ mix('js/appv2.js') }}"></script>
-    </body>
+
+    <link href="{{ asset('css/normalize.css?v=1.1') }}" rel="stylesheet">
+    <link href="{{ asset('css/appv1.css?v=1.1') }}" rel="stylesheet">
+</head>
+
+<body class="theme-{{ config('EVENT_TYPE_THEME', 'htf') }}">
+    <div id="app">
+
+        <app></app>
+    </div>
+    <script>
+        window.locale = '{{ app()->getLocale() }}';
+
+        window.appConfigs = {
+            'theme': "{{ config('EVENT_TYPE_THEME', 'htf') }}"
+        }
+    </script>
+
+    <script src="{{ mix('js/appv2.js') }}"></script>
+</body>
+
 </html>
